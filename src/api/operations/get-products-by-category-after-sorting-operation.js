768d56a7bfb98f3052296f36fd81fac85cd566b7
Anatoli <saukhin.anatoli@gmail.com>
2024-11-16 14:22:32 +0100

    fix product list

---
+++ src/api/operations/get-products-by-category-after-sorting-operation.js
@@ -0,0 +1,9 @@
+// import { getProductsByCatrgoryAfterSorting } from "../fetch/get-products-by-category-after-sorting";
+
+// export const getProductsByCategoryAfterSorting = async (
+//   searchPhrase,
+//   categoryId
+// ) => {
+//   const products = getProductsByCatrgoryAfterSorting(searchPhrase, categoryId);
+//   return products;
+// };
